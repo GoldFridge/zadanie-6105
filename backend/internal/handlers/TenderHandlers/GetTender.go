c2832e7afe5cc20a886012d4d02978ba97a73e44
Fridge <fridge.1002@gmail.com>
2024-09-12 16:19:18 +0600

    swagger docs

--- backend/internal/handlers/TenderHandlers/GetTender.go
+++ backend/internal/handlers/TenderHandlers/GetTender.go
@@ -22,7 +22,7 @@ func GetTender(w http.ResponseWriter, r *http.Request) {
 	var tenders []models.Tender
 
 	// Execute raw SQL query and scan results into tenders slice
-	if err := db.Raw("SELECT * FROM tenders").Scan(&tenders).Error; err != nil {
+	if err := db.Raw("SELECT * FROM tender").Scan(&tenders).Error; err != nil {
 		log.Printf("Error fetching data from database: %v", err)
 		http.Error(w, "Error fetching data", http.StatusInternalServerError)
 		return
